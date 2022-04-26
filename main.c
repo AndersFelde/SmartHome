bbe58c1abd586db6aee0f523de41969f06a470e8
GitExample <git@example.com>
2022-04-26 08:11:47 +0200

    fikset mappe struktur

--- main.c
+++ main.c
@@ -3,16 +3,13 @@
  *
  * Created: 26.04.2022 08:00:23
  * Author : Anders K Felde
- */ 
+ */
 
 #include <avr/io.h>
 
-
-int main(void)
-{
+int main(void) {
     /* Replace with your application code */
-    while (1) 
-    {
+
+    while (1) {
     }
 }
-
