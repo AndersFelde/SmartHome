ed8e691d8407bfae9ef930a3c3316c55ac527c7f
GitExample <git@example.com>
2022-05-04 10:07:47 +0200

    funker jævla bra

---
+++ USART.c
@@ -0,0 +1,56 @@
+/*
+ * USART.c
+ *
+ * Created: 26.04.2022 09:26:20
+ *  Author: Anders K Felde
+ */
+
+/*
+ * USART.c
+ *
+ * Created: 28.03.2022 12:43:56
+ * Author : Anders K Felde
+ */
+
+#include <avr/io.h>
+
+void USART_Init(unsigned int baud) {
+    // Put the upper part of the baud number here (bits 8 to 11)
+    UBRRH = (unsigned char)(baud >> 8);
+
+    // Put the remaining part of the baud number here
+    UBRRL = (unsigned char)baud;
+
+    // Enable the receiver and transmitter
+    UCSRB = (1 << RXEN) | (1 << TXEN);
+
+    // Set 2 stop bits and data bit length is 8-bit
+    UCSRC = (1 << URSEL) | (1 << USBS) | (3 << UCSZ0);
+}
+
+void USART_Transmit(unsigned int data) {
+    // Wait until the Transmitter is ready
+    while (!(UCSRA & (1 << UDRE)))
+        ;
+
+    // Make the 9th bit 0 for the moment
+    UCSRB &= ~(1 << TXB8);
+
+    // If the 9th bit of the data is a 1
+    if (data & 0x0100) {
+        // Set the TXB8 bit to 1
+        UCSRB |= (1 << TXB8);
+    }
+
+    // Get that data outa here!
+    UDR = data;
+}
+
+char USART_Receive(void) {
+    if (!(UCSRA & (1 << RXC))) {
+        return '-';
+    } // Wait for the RXC to not have 0
+      /*else {*/
+    return UDR;
+    //} // Get that data outa there and back to the main program!
+}
--- main.c
+++ main.c
@@ -3,16 +3,35 @@
  *
  * Created: 04.05.2022 09:07:24
  * Author : Anders K Felde
- */ 
+ */
 
+#include "includes/USART.h"
 #include <avr/io.h>
 
-
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+void startAlarm() {
+    PORTB |= (1 << PINB0);
+    PORTB |= (1 << PINB1);
+}
+void stopAlarm() {
+    PORTB &= ~(1 << PINB0);
+    PORTB &= ~(1 << PINB1);
 }
 
+int main(void) {
+    /* Replace with your application code */
+    USART_Init(2400);
+    char receiveChar;
+
+    DDRB |= (1 << PINB0);
+    DDRB |= (1 << PINB1);
+
+    while (1) {
+        receiveChar = USART_Receive();
+
+        if (receiveChar == '1') {
+            startAlarm();
+        } else if (receiveChar == '0') {
+            stopAlarm();
+        }
+    }
+}
