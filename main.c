77206e794fe0f0d09cb3dca8ebc54ef6d6322bdc
AndersFelde <anderskfelde@hotmail.com>
2022-05-22 17:04:56 +0200

    oppdatert versjon

--- main.c
+++ main.c
@@ -9,34 +9,45 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void startAlarm() {
+void startAlarm()
+{
+    // sender strøm på pins som er koblet til høyttaler
     PORTB |= (1 << PINB0);
-    PORTB &= ~(1 << PINB1);
     PORTB |= (1 << PINB1);
 }
-void stopAlarm() {
+
+void stopAlarm()
+{
+    // sender ikke strøm på pins som er koblet til høyttaler
     PORTB &= ~(1 << PINB0);
     PORTB &= ~(1 << PINB1);
 }
 
-int main(void) {
-    /* Replace with your application code */
+int main(void)
+{
     USART_Init(2400);
     char receiveChar;
+
+    // setter høyttaler PINS til å være output
     DDRB |= 1 << PINB0;
     DDRB |= 1 << PINB0;
 
-    while (1) {
-        PORTB ^= 1 << PINB0;
-        _delay_ms(100);
-        PORTB ^= 1 << PINB1;
-        _delay_ms(100);
+    while (1)
+    {
+        // for å spille lyd i ujevn takt
+        // PORTB ^= 1 << PINB0;
+        // _delay_ms(100);
+        // PORTB ^= 1 << PINB1;
+        // _delay_ms(100);
 
         receiveChar = USART_Receive();
 
-        if (receiveChar == '1') {
+        if (receiveChar == '1')
+        {
             startAlarm();
-        } else if (receiveChar == '0') {
+        }
+        else if (receiveChar == '0')
+        {
             stopAlarm();
         }
     }
